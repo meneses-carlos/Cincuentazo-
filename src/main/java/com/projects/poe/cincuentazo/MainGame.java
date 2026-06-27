371ffba01efd14248f154b08d62f90e41c680613
sant2111 <s4nt2ll1@gmail.com>
2026-06-27 17:10:43 -0500

    style: improve IU for mainGameView.fxml and startMenuView.fxml.

--- src/main/java/com/projects/poe/cincuentazo/MainGame.java
+++ src/main/java/com/projects/poe/cincuentazo/MainGame.java
@@ -10,13 +10,16 @@ import java.io.IOException;
 public class MainGame extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MainGame.class.getResource("mainGameView.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1536, 1024);
+        FXMLLoader fxmlLoader = new FXMLLoader(MainGame.class.getResource("startMenuView.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 700, 600);
         stage.setTitle("50tazo");
         stage.setScene(scene);
         stage.setResizable(false);
         stage.show();
 
-
+        // no esta hecha la transicion del inicio al principal si quieren verlos cambien el get resourse por el nombre del que quiere ver
+        //ajusten el ancho y largo
+        //main game 1048x1024
+        //start menu 700x600
     }
 }
