2c903a80a334526799a8216efa60ffabbadc8b38
Carlos Meneses <menesesllantencarlosandres@gmail.com>
2026-06-29 17:14:20 -0500

    feat: implement menu navigation and controller skeleton

--- src/main/java/com/projects/poe/cincuentazo/controller/StartMenuController.java
+++ src/main/java/com/projects/poe/cincuentazo/controller/StartMenuController.java
@@ -5,6 +5,11 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class StartMenuController {
 
@@ -30,19 +35,42 @@ public class StartMenuController {
 
 
     // Validate menu input
-    public void validateSelection() {
+    public boolean validateSelection(){
+        if(numberOfPlayersComboBox.getValue() == null){
+            showWarning("Selecciona para jugar");
+            return false;
+        }
+        hideWarning();
 
+        return true;
     }
+
 
     // Start the game
     public void startGame() {
 
+        if (validateSelection()) {
+            loadGameView();
+        }
     }
 
     // Load the game scene
     public void loadGameView() {
 
+    try {
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/projects/poe/cincuentazo/mainGameView.fxml"));
+        Scene scene = new Scene(loader.load());
+        Stage stage = (Stage) playButton.getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
     }
+
+    catch (IOException e) {
+
+        e.printStackTrace();
+    }
+}
 
     // Display a warning message
     public void showWarning(String message) {
